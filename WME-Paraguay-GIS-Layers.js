640497a9ebd5dcac31bcf3d4e25e2a2cffe7932c
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2023-03-30 17:16:49 -0400

    adding connect points

--- WME-Paraguay-GIS-Layers.js
+++ WME-Paraguay-GIS-Layers.js
@@ -3,7 +3,7 @@
 // ==UserScript==
 // @name         WME Paraguay GIS Layers
 // @namespace    https://greasyfork.org/users/324334
-// @version      2023.03.21.001-py024
+// @version      2023.03.21.001-py025
 // @description  Adds Paraguay GIS layers in WME
 // @author       MapOMatic
 // @match         *://*.waze.com/*editor*
@@ -49,6 +49,7 @@
 // @connect vigisalud.gov.py
 // @connect mapaescolar.mec.gov.py
 // @connect apps.mades.gov.py
+// @connect www.mopc.gov.py
 // ==/UserScript==
 
 // This version is for Paraguay Only, modified by ancho85
